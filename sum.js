f2f2777e936bbd42d575166bd4c21ff5988eec18
MohammeddJaveed <mdjaveed9108gmail.com>
2025-01-05 23:25:04 +0000

    two numbers in an array that add up to a target

---
+++ sum.js
@@ -0,0 +1,16 @@
+// Find two numbers in an array that add up to a target
+let target=50;
+const arr=[20,30,25,25,30]
+let result=[]
+function targetSum(){
+    // Take first elemet from first loop and check adding the element with other using second for loop
+    for(let i=0;i<=arr.length;i++){
+        for(let j=i+1;j<=arr.length;j++){
+            if(arr[i] + arr[j]===target){
+                result.push([arr[i], arr[j]]); 
+            }
+        }
+    }
+    console.log(result)
+}
+targetSum()
