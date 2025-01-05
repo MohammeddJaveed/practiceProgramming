29629a00c58e84ac047a8cee7be578f612312546
MohammeddJaveed <mdjaveed9108gmail.com>
2025-01-05 23:45:03 +0000

    Rotate an array to the right by k steps

---
+++ rotateArray.js
@@ -0,0 +1,12 @@
+// Rotate an array to the right by k steps
+const arr=[1,2,3,4,5,6,7]
+const k = 9
+function arrayRotation(arr,k){
+    k=k%7 // divide number of steps by total number of elements in array
+    // we have to loop till the number of rotations
+    for(i=0;i<k;i++){
+        arr.unshift(arr.pop())
+    }
+    console.log(arr)
+}
+arrayRotation(arr,k)
