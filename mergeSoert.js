af2619b19304dcd84a057db8436ee6cf8458f0e7
MohammeddJaveed <mdjaveed9108gmail.com>
2025-01-06 16:27:59 +0000

    mergesort

---
+++ mergeSoert.js
@@ -0,0 +1,8 @@
+const arr1=[100,200,300,400]
+const arr2=[1,2,3,4,5,7,200,300,900]
+function combineArray(){
+    let comined= arr1.concat(arr2);
+    comined.sort((a, b) => a - b)
+    return comined
+}
+console.log(combineArray())
