9e4d9a262eb3ae6169683a6b0e56abc94f5a990e
MohammeddJaveed <mdjaveed9108gmail.com>
2025-01-06 14:53:34 +0000

    Binary search

---
+++ binarySearch.js
@@ -0,0 +1,22 @@
+const arr=[1,2,3,4,5,6,7,8,9]
+let target =9
+function binarySearch(){
+    let low=0
+    let high= arr.length-1
+    let mid
+    while(low<=high){
+        mid=Math.floor((low+high)/2)
+        if(target===arr[mid]){
+            return(`target found at ${mid}`)
+        }
+        if(arr[mid]>target){
+            high = mid-1
+        }else{
+            low=mid+1
+        }
+    }
+    return -1
+    
+    
+}
+console.log(binarySearch())
