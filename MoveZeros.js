9af464655685110794fdc8578510e2fe8d0d8789
MohammeddJaveed <mdjaveed9108gmail.com>
2025-01-06 13:54:41 +0000

    movezeros

---
+++ MoveZeros.js
@@ -0,0 +1,20 @@
+const arr = [1, 2, 3, 0, 10, 22, 0, 1, 0];
+
+function moveZeros() {
+   let result =[];
+   let zeroCount=0;
+   for(let i=0;i<arr.length;i++){
+    if(arr[i]===0){
+        zeroCount++
+    }else{
+        result.push(arr[i])
+    }
+   }
+   while(zeroCount>0){
+    result.push(0);
+    zeroCount--
+   }
+   return result
+}
+
+console.log(moveZeros());
