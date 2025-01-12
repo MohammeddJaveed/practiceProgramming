f7d418f06c9457cf509caa8fc43d5d045bc29b41
MohammeddJaveed <mdjaveed9108gmail.com>
2025-01-12 23:59:12 +0000

    minmax

---
+++ Array Problems/minmax.js
@@ -0,0 +1,26 @@
+// Find min and max element in an array
+//Time Complexity - O(n)
+//space Complexity - O(1)
+
+let arr = [1,2,3,4,5,6,7,8,9,0,-1]
+
+function minmax(){
+    let min = arr[0];
+    let max = arr[0];
+
+    for(let i =0; i <= arr.length; i++){
+
+        if(arr[i] > max){
+            max = arr[i]
+           
+        }
+
+        if(arr[i] < min){
+            min = arr[i]
+          
+        }
+    }
+    return {min , max}
+   
+}
+console.log(minmax())
