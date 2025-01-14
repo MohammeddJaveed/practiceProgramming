63ed24c541f8d1a1588c4ee828d789ccdb206c82
MohammeddJaveed <mdjaveed9108gmail.com>
2025-01-14 22:15:21 +0000

    completed todays 2 programs

--- Array Problems/Posted/frequency.js
+++ Array Problems/Posted/frequency.js
@@ -4,16 +4,16 @@
 // space Complexity - O(k) ,k is number of unique elements in array
 
 
-const arr = [1, 2, 2, 3, 4, 3, 3, 5];
+const arr = [1, 2, 2, 3, 4, 3, 5, 99];
 
 function findFrequencies(array) {
     const frequencies = {};
 
     for (let i = 0; i< arr.length; i++) {
-        if (frequencies[array[i]]) {
-            frequencies[array[i]]++; // Increment count if the number already exists
+        if (frequencies[arr[i]]) {
+            frequencies[arr[i]]++; // Increment count if the number already exists
         } else {
-            frequencies[array[i]] = 1; // Initialize count to 1 if it's the first occurrence
+            frequencies[arr[i]] = 1; // Initialize count to 1 if it's the first occurrence
         }
     }
 
--- Array Problems/Posted/mergeSort.js
+++ Array Problems/Posted/mergeSort.js
@@ -6,7 +6,7 @@
 const arr1=[100,200,300,400]
 const arr2=[1,2,3,4,5,7,200,300,900]
 function combineArray(){
-    let comined= arr1.concat(arr2);
+    let comined= [... new Set (arr1.concat(arr2))];
     comined.sort((a, b) => a - b)
     return comined
 }
